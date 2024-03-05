2ffb0c4f747f434f26bef66fb2cc5f698f19f7fd
joris <joris@DESKTOP-6JFGPOU>
2024-03-06 00:32:22 +0530

    First api test

--- Scripts/Get_List of users/Script1709664990781.groovy
+++ Scripts/Get_List of users/Script1709664990781.groovy
@@ -17,5 +17,7 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-WS.sendRequest(findTestObject(null))
+WS.sendRequest(findTestObject('API/Rest/GET -List users'))
+
+WS.sendRequestAndVerify(findTestObject('API/Rest/GET -List users'))
 
